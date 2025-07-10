d850b79491af632d8909185dedcbbcddeb0b05fb
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-07-10 14:10:18 -0300

    pip config

--- pipe_client/client.py
+++ pipe_client/client.py
@@ -6,7 +6,7 @@ import os
 
 address = r'\\.\pipe\macro-manager-pipe' 
 
-ipc_adress_key = os.getenv("pipe_key").encode()
+ipc_adress_key = b"1e0dc389-f3b9-4da4-b429-0906277d7545"
 
 conn = multiprocessing.connection.Client(address, authkey=ipc_adress_key)
 _json = {
